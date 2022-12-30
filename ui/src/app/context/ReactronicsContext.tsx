8bebe4f39171a2854dc57deb98ecb1c5a04cd832
xiaoran-MENG <xrmeng720@gmail.com>
2022-12-30 02:37:37 -0600

    Set up context

---
+++ ui/src/app/context/ReactronicsContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+import { Cart } from "../models/cart";
+
+interface State {
+    cart: Cart | null
+    setCart: (c: Cart) => void
+    remove: (id: number, quantity: number) => void
+}
+
+export const ReactronicsContext = createContext<State | undefined>(undefined)
+
+export const useReactronicsContext = () => {
+    const c = useContext(ReactronicsContext)
+    if (!c) throw Error("No provider");
+    return c
+}
+
+export const ReactronicsContextProvider = ({ children }: PropsWithChildren<any>) => {
+    const [cart, setCart] = useState<Cart | null>(null)
+
+    const remove = (id: number, quantity: number) => {
+        if (!cart) return;
+
+        const items = [...cart.items]
+        const i = items.findIndex(x => x.productId == id)
+        if (i < 0) return;
+
+        items[i].quantity -= quantity;
+        if (items[i].quantity <= 0) items.splice(i, 1)
+        
+        setCart(c => {
+            return { ...c!, items}
+        })
+    }
+
+    return <ReactronicsContext.Provider value={{
+        cart,
+        setCart,
+        remove
+    }}>{children}</ReactronicsContext.Provider>
+}
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ReactronicsContextProvider } from './app/context/ReactronicsContext';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <ReactronicsContextProvider>
       <App />
+    </ReactronicsContextProvider>
     </BrowserRouter> 
   </React.StrictMode>
 );
